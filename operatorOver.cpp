02ebdfd17b0d41f66d29edfa3c8a4a9655040e48
saakshiBmsce <53811241+saakshiBmsce@users.noreply.github.com>
2020-01-23 15:23:07 +0530

    Add files via upload

---
+++ operatorOver.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//class Complex
+//{
+//	public:int real;
+//	int img;
+//	Complex operator+(Complex c)
+//	{
+//		Complex res;
+//		res.real=real+c.real;
+//		res.img=img+c.img;
+//		return res;
+//	}
+//	void dis()
+//	{
+//		cout<<real<<" + i"<<img<<"\n";
+//	}
+//};
+//
+//int main()
+//{
+//	Complex c1;
+//	c1.real=5;
+//	c1.img=6;
+//	Complex c2;
+//	c2.real=4;
+//	c2.img=9;
+//	c1.dis();cout<<" + ";c2.dis();
+//	Complex c3;
+//	c3=c1+c2;
+//	c3.dis();
+//}
